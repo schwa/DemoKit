0dea070047d88f72435dc1bc61c83953084971a7
Jonathan Wight <jwight@mac.com>
2022-08-19 14:52:23 -0700

    WIP

---
+++ App/DemoKitApp/ContentView.swift
@@ -0,0 +1,158 @@
+import SwiftUI
+@_spi(DemoKit) import DemoKit
+
+struct ContentView: View {
+    var body: some View {
+        UserDefaultsView()
+    }
+}
+
+
+struct UserDefaultsView: View {
+
+    struct Record: Identifiable {
+        let id: [AnyHashable]
+        let key: String
+
+        enum Value {
+            case boolean(Bool)
+            case integer(Int)
+            case float(Double)
+            case string(String)
+            case array([Any])
+            case dictionary([String: Any])
+            case data(Data)
+            case date(Date)
+            case unknown(Any)
+
+            var typeName: String {
+                let type: String
+                switch self {
+                case .boolean:
+                    type = "boolean"
+                case .integer:
+                    type = "integer"
+                case .float:
+                    type = "float"
+                case .string:
+                    type = "string"
+                case .array:
+                    type = "array"
+                case .dictionary:
+                    type = "dictionary"
+                case .data:
+                    type = "data"
+                case .date:
+                    type = "date"
+                case .unknown:
+                    type = "unknown"
+                }
+                return type
+            }
+        }
+        let value: Value
+    }
+
+    @State
+    var records: [Record]
+
+    @State
+    var filteredRecords: [Record]
+
+    @State
+    var searchText: String = ""
+
+    init() {
+        let items: [(key: String, value: Any)] = Array(UserDefaults.standard.dictionaryRepresentation())
+        let records = items.enumerated().map { (index, item) in
+            let value: Record.Value
+            switch item.value {
+            case let o as Bool:
+                value = .boolean(o)
+            case let o as Int:
+                value = .integer(o)
+            case let o as Double:
+                value = .float(o)
+            case let o as String:
+                value = .string(o)
+            case let o as Data:
+                value = .data(o)
+            case let o as Date:
+                value = .date(o)
+            case let o as Array<Any>:
+                value = .array(o)
+            case let o as Dictionary<String, Any>:
+                value = .dictionary(o)
+            default:
+                value = .unknown(item.value)
+            }
+            return Record(id: [index], key: item.key, value: value)
+        }
+        let filteredRecords = records.sorted(using: MySortComparator(\.key))
+
+        self.records = records
+        self.filteredRecords = filteredRecords
+    }
+
+    var body: some View {
+        Table(filteredRecords) {
+            TableColumn("Key", value: \.key)
+            TableColumn("Type") { record in
+                return Text(verbatim: record.value.typeName)
+            }
+            TableColumn("Value") { record in
+                switch record.value {
+                case .boolean(let value):
+                    Toggle(isOn: .constant(value)) {
+                        EmptyView()
+                    }
+                    .toggleStyle(SwitchToggleStyle())
+                case .integer(let value):
+                    Text(value, format: .number).monospacedDigit()
+                case .float(let value):
+                    Text(value, format: .number).monospacedDigit()
+                case .string(let value):
+                    Text(verbatim: value)
+                case .array(let value):
+                    ArrayView(array: value)
+                case .dictionary(let value):
+                    Text(verbatim: String(describing: value))
+                case .data(let value):
+                    Text("\(value.count) bytes")
+                case .date(let value):
+                    Text(value, style: .date)
+                case .unknown(let value):
+                    Text(verbatim: String(describing: value))
+                }
+            }
+        }
+        .searchable(text: $searchText)
+        .onChange(of: searchText) { searchText in
+            let pattern = Regex { searchText }.ignoresCase()
+            let filteredRecords = records
+                .filter { record in
+                    if searchText.isEmpty {
+                        return true
+                    }
+                    return record.key.contains(pattern)
+                    || String(describing: record.value).contains(pattern)
+                }
+                .sorted(using: MySortComparator(\.key))
+
+            self.filteredRecords = filteredRecords
+        }
+    }
+}
+
+struct ArrayView: View {
+    let array: [Any]
+
+    var body: some View {
+        let array = Array(array.enumerated())
+        LazyVStack(alignment: .leading) {
+            ForEach(array, id: \.0) { element in
+                Text(verbatim: String(describing: element.1))
+            }
+        }
+    }
+}
--- Sources/DemoKit/DemoKit.swift
+++ Sources/DemoKit/DemoKit.swift
@@ -1,6 +1,243 @@
-public struct DemoKit {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
+import Foundation
+import RegexBuilder
+import os
+#if os(macOS)
+import AppKit
+#endif
+import AsyncAlgorithms
 
-    public init() {
+let logger = Logger(subsystem: "TODO", category: "TODO")
+
+struct DemosView: View {
+
+    @Environment(\.scenePhase)
+    var scenePhase
+
+    @StateObject
+    var model = DemoModel {
+        AnyDemo("Red Demo") {
+            Color.red
+        }
+        AnyDemo("Green Demo") {
+            Color.green
+        }
+        AnyDemo("Blue Demo") {
+            Color.blue
+        }
+    }
+
+    @AppStorage("selectedDemo") // TODO: Should be SceneStorage
+    var sidebarSelection: String = ""
+
+    @State
+    var searchText: String = ""
+
+    var filteredDemos: [AnyDemo] {
+        let pattern = Regex { searchText }.ignoresCase()
+        return Array(model.allDemos.values
+            .filter { searchText.isEmpty || $0.title.contains(pattern) }
+            .sorted(using: MySortComparator(\.title)))
+    }
+
+    init() {
+        logger.log("INIT")
+    }
+
+    var body: some View {
+        NavigationSplitView {
+            List(selection: $sidebarSelection) {
+                ForEach(filteredDemos) { demo in
+                    DemoRow(demo: demo, metadata: model.demoMetadata[demo.id]!)
+                }
+            }
+            .searchable(text: $searchText, placement: .sidebar, prompt: "search…")
+        } detail: {
+            if let demo = model.allDemos[sidebarSelection] {
+                CrashDetectionView(id: demo.id, showLifeCycle: true) {
+                    DemoView(demo: demo)
+                }
+                .id(demo.id)
+            }
+        }
+        .environmentObject(model)
+        .onChange(of: scenePhase) { scenePhase in
+            logger.log("\(String(describing: scenePhase))")
+        }
+#if os(macOS)
+        //        .task {
+        //            do {
+        //                let n = NotificationCenter.default.notifications(named: NSApplication.willTerminateNotification)
+        //                for try await x in n {
+        //                    logger.log("\(x)")
+        //                }
+        //            }
+        //            catch {
+        //            }
+        //        }
+#endif
+    }
+}
+
+struct DemoView: View {
+    @EnvironmentObject
+    var model: DemoModel
+
+    let demo: AnyDemo
+
+    var body: some View {
+        demo.body
+    }
+}
+
+// TODO: Doesn't wor
+struct DemoRow: View {
+
+    @EnvironmentObject
+    var model: DemoModel
+
+    let demo: AnyDemo
+
+    @State
+    var metadataDisplayed = false
+
+    @State
+    var metadata: DemoMetadata
+
+    var body: some View {
+        return HStack {
+            Toggle("Star", isOn: $metadata.starred)
+                .toggleStyle(MyToggleStyle(on: "star.fill", off: "star"))
+            Text(demo.title)
+            Spacer()
+            Button {
+                metadataDisplayed = true
+            } label: {
+                Image(systemName: "info.circle")
+            }
+            .buttonStyle(.borderless)
+            .popover(isPresented: $metadataDisplayed) {
+                let metadata = model.demoMetadata[demo.id]! // TODO: Bang
+                Form {
+                    LabeledContent("ID", value: demo.id)
+                    LabeledContent("Title", value: demo.title)
+                    LabeledContent("Tags", value: metadata.tags.joined(separator: ", ")) // TODO: list format
+                    LabeledContent("Comments") {
+                        TextField("comment", text: $metadata.comments)
+                    }
+                    TimelineView(.animation) { time in
+                        LabeledContent("Last Launched") {
+                            if let value = metadata.lastLaunched {
+                                Text(value, format: .dateTime)
+                            }
+                        }
+                        LabeledContent("Last Duration") {
+                            if let value = metadata.lastDuration {
+                                Text(Duration(value), format: .duration)
+                                //                                Text(value, format: .number)
+                            }
+                        }
+                    }
+                }
+                .padding()
+            }
+        }
+        .onChange(of: metadata) { newValue in
+            model.demoMetadata[demo.id] = metadata
+        }
+    }
+}
+
+
+// MARK: -
+
+class DemoModel: ObservableObject {
+
+    @Published
+    var allDemos: [String: AnyDemo] = [:]
+
+    @Published
+    //    var demoMetadata: [String: DemoMetadata] = [:]
+    var demoMetadata = try! MiniDatabase<DemoMetadata>(url: FileManager().applicationSupportDirectory.appending(path: "test.journal"))
+}
+
+@resultBuilder
+struct DemosBuilder {
+    static func buildBlock(_ components: any Demo...) -> [AnyDemo] {
+        return components.map { AnyDemo($0) }
+    }
+
+}
+
+extension DemoModel {
+    convenience init(@DemosBuilder _ demos: () -> [AnyDemo]) {
+        let demos = demos()
+        self.init()
+        for demo in demos {
+            allDemos[demo.id] = demo
+            demoMetadata[demo.id] = .init(demo: demo.id, tags: [], comments: "", lastLaunched: nil, lastDuration: nil)
+        }
+    }
+}
+
+protocol Demo: Identifiable {
+    associatedtype Content where Content: View
+    var id: String { get }
+    var title: String { get }
+    var body: Content { get }
+}
+
+struct AnyDemo: Demo {
+    let id: String
+    let title: String
+    let view: () -> AnyView
+
+    init <Base>(_ base: Base) where Base: Demo {
+        self.id = base.id
+        self.title = base.title
+        self.view = {
+            AnyView(base.body)
+        }
+    }
+
+    var body: some View {
+        return view()
+    }
+}
+
+extension AnyDemo {
+    init <Content>(_ title: String, @ViewBuilder body: @escaping () -> Content) where Content: View {
+        self.id = title
+        self.title = title
+        self.view = {
+            AnyView(body())
+        }
+    }
+}
+
+struct DemoMetadata: Equatable, Codable {
+    let demo: String
+    var tags: Set<String>
+    var comments: String
+    var lastLaunched: Date?
+    var lastDuration: TimeInterval?
+}
+
+extension DemoMetadata {
+    var starred: Bool {
+        get {
+            return tags.contains("starred")
+        }
+        set {
+            if newValue {
+                logger.log("INSERTING")
+                tags.insert("starred")
+            }
+            else {
+                logger.log("REMOVING")
+                tags.remove("starred")
+            }
+
+        }
     }
 }
