65339fa289c97d7aa8d5f734db4ff59b7cfc0aec
Jonathan Wight <jwight@mac.com>
2026-04-03 14:16:04 -0700

    Remove generic DemoView1-4 placeholder demos

--- Demo/Demo/DemoApp.swift
+++ Demo/Demo/DemoApp.swift
@@ -9,10 +9,6 @@ struct DemoApp: App {
 
     var body: some Scene {
         DemoPickerScene(demos: [
-            DemoView1.self,
-            DemoView2.self,
-            DemoView3.self,
-            DemoView4.self,
             StateTestDemoView.self,
             CirclesDemoView.self,
             RoundedPolygonDemoView.self,
